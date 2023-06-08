9c689b1f0d5d2e72b176090ae14837d4d6949956
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-06-08 23:17:11 +0530

    input handler

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -5,9 +5,17 @@ import { ExpContext } from "../calculator/Calculator";
 
 const Display = () => {
     let {exp,setExp} = useContext(ExpContext);
-    
+
+    let handleInput = (e) => {        
+        let temp = e.target.value;
+
+        if(temp.charAt(temp.length-1).match(/[0-9+\-*./%]/g) || temp===""){
+            setExp(temp);
+        }
+    };
+
     return(
-        <input className="display" value={exp}/>
+        <input className="display" value={exp} onInput={handleInput}/>
     );
 }
 
