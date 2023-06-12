1202c67a35af6534e4967c983fdfd1aaaed039a4
Dhanush-Karthik <dhanushkarthikgit@gmail.com>
2023-06-12 16:34:47 +0530

    class name modified

--- src/components/numberpad/NumberPad.js
+++ src/components/numberpad/NumberPad.js
@@ -4,90 +4,86 @@ import "./NumberPad.css";
 let keys = [
   {
     value: "C",
-    class: "number-pad-item spl",
+    class: " spl",
   },
   {
     value: "BS",
-    class: "number-pad-item spl",
+    class: " spl",
   },
   {
     value: "%",
-    class: "number-pad-item op",
+    class: " op",
   },
   {
     value: "/",
-    class: "number-pad-item op",
+    class: " op",
   },
   {
     value: "7",
-    class: "number-pad-item",
   },
   {
     value: "8",
-    class: "number-pad-item",
   },
   {
     value: "9",
-    class: "number-pad-item",
   },
   {
     value: "*",
-    class: "number-pad-item op",
+    class: " op",
   },
   {
     value: "4",
-    class: "number-pad-item",
   },
   {
     value: "5",
-    class: "number-pad-item",
   },
   {
     value: "6",
-    class: "number-pad-item",
   },
   {
     value: "-",
-    class: "number-pad-item op",
+    class: " op",
   },
   {
     value: "1",
-    class: "number-pad-item",
   },
   {
     value: "2",
-    class: "number-pad-item",
   },
   {
     value: "3",
-    class: "number-pad-item",
   },
   {
     value: "+",
-    class: "number-pad-item op",
+    class: " op",
   },
   {
     value: "0",
-    class: "number-pad-item unique",
+    class: " unique",
   },
   {
     value: ".",
-    class: "number-pad-item",
   },
   {
     value: "=",
-    class: "number-pad-item op",
+    class: " op",
   },
 ];
 
 let NumberPad = () => {
 
   return (
-      <div className="number-pad">
-        {keys.map((key, index) => {
-          return <Button key={index} value={key.value} className={key.class} />;
-        })}
-      </div>
+    <div className="number-pad">
+      {keys.map((key, index) => {
+        return (
+          <Button
+            key={index}
+            value={key.value}
+            className={(key.class) ? "number-pad-item"+key.class : "number-pad-item"}
+          />
+        );
+      })}
+    </div>
   );
 };
 
